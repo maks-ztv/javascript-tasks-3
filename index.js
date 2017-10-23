4505843b6f9798ca592ae0c6243ba399eaad9739
Oleksandr Bugaenko <aleksandr@admanic.com>
2017-10-23 17:09:09 +0300

    task-potato party

--- index.js
+++ index.js
@@ -15,35 +15,39 @@ var result = lego.query(
     // И выбираем только нужные поля
     lego.select('name', 'gender', 'age', 'phone', 'favoriteFruit'),
 
+    //
     // Обязательно выбираем тех, кто любит Яблоки и Картофель (самое важное !!!)
     lego.filterIn('favoriteFruit', ['Яблоко', 'Картофель']),
-
+    //
     // Отсортируем их по возрасту (но зачем?)
     lego.sortBy('age', 'asc'), // Бывает только asc (от меньшего к большему) или desc (наоборот)
-
+    //
     // А пол выведем только первой буквой для удобства
-    lego.format('gender', function (value) {
-        return value[0];
+    lego.format(function (el) {
+        return Object.assign(el, {gender: el.gender[0]});
     }),
-
-    // На дачу влезет примерно 10 человек
+    // lego.format('gender', function (value) {
+    //     return value[0];
+    // }),
+    //
+    //На дачу влезет примерно 10 человек
     lego.limit(10)
 );
 
 // Будет круто организовать две вечеринки сразу: яблочную для девушек и картофельную для парней.
 
-var result = lego.query(
-    phoneBook,
-
-    // Выбираем всех парней, которые любят картофель, и всех девушек, которые любят яблоки
-    lego.or(
-        lego.and(
-            lego.filterEqual('gender', 'Мужской'),
-            lego.filterIn('favoriteFruit', ['Картофель'])
-        ),
-        lego.and(
-            lego.filterEqual('gender', 'Женский'),
-            lego.filterIn('favoriteFruit', ['Яблоко'])
-        )
-    )
-);
+// var result = lego.query(
+//     phoneBook,
+//
+//     // Выбираем всех парней, которые любят картофель, и всех девушек, которые любят яблоки
+//     lego.or(
+//         lego.and(
+//             lego.filterEqual('gender', 'Мужской'),
+//             lego.filterIn('favoriteFruit', ['Картофель'])
+//         ),
+//         lego.and(
+//             lego.filterEqual('gender', 'Женский'),
+//             lego.filterIn('favoriteFruit', ['Яблоко'])
+//         )
+//     )
+// );
--- lego.js
+++ lego.js
@@ -1,27 +1,71 @@
 'use strict';
 
 // Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function (collection /* операторы через запятую */) {
-
+module.exports.query = function (collection, ...funcs /* операторы через запятую */) {
+    funcs.forEach(function (f) {
+        collection = f(collection);
+    });
+    console.log(collection);
+    // console.log(funcs[](collection));
 };
 
 // Оператор reverse, который переворачивает коллекцию
-module.exports.reverse = function () {
-    return function (collection) {
-        var changedCollection = collection.reverse();
-
-        // Возращаем изменённую коллекцию
-        return changedCollection;
-    };
-};
-
-// Оператор limit, который выбирает первые N записей
-module.exports.limit = function (n) {
-    // Магия
-};
+// module.exports.reverse = function () {
+//     return function (collection) {
+//         var changedCollection = collection.reverse();
+//
+//         // Возращаем изменённую коллекцию
+//         return changedCollection;
+//     };
+// };
 
 // Вам необходимо реализовать остальные операторы:
 // select, filterIn, filterEqual, sortBy, format, limit
 
-// Будет круто, если реализуете операторы:
-// or и and
+//Select function
+module.exports.select = function (...args) {
+    return function (array) {
+        return array.map(function (el) {
+            var obj = {};
+            Object.keys(el).forEach(function (key) {
+                if (args.indexOf(key) !== -1) {
+                    obj[key] = el[key];
+                }
+            });
+            return obj;
+        });
+    };
+};
+
+//FilterIn function
+module.exports.filterIn = function (key, values) {
+  return function (array) {
+      return array.filter(function (el) {
+          return values.indexOf(el[key]) !== -1;
+      });
+  };
+};
+
+//SortBy function
+module.exports.sortBy = function (key, order) {
+    return function (array) {
+        return array.sort(function (a, b) {
+            return order == 'asc' ? a[key] - b[key] : b[key] - a[key];
+        });
+    };
+};
+
+//Format function
+module.exports.format = function (f) {
+    return function (array) {
+        return array.map(f);
+    };
+};
+
+//Limit function
+module.exports.limit = function (n) {
+    return function (array) {
+        array.length = n;
+        return array;
+    };
+};
